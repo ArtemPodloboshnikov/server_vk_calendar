e707953e96f89ddb80437bf58b866420c334f92b
Artem Podloboshnikov <ArtemPS777@yandex.ru>
2021-10-03 03:38:42 +0300

    Next level

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const fileContents = fs.readFileSync('./data.json', 'utf8');
 function getData(req, res, next) {
   try {
     const data = JSON.parse(fileContents)
-    console.log(data)
+    console.log(data);
     res.send((req.params.date == 'all')?JSON.stringify(data):JSON.stringify(data[req.params.date]));
   } catch(err) {
     console.error(err)
@@ -17,11 +17,12 @@ function getData(req, res, next) {
 function setData(req, res, next) {
     try {
       let data = JSON.parse(fileContents);
-      data[req.params.date] = req.params.events.split(',');
+      data[req.params.date] = Object.keys(req.body)[0].split('.');
       fs.writeFileSync('./data.json', JSON.stringify(data))
-      res.send('Ок');
+      res.statusCode(200);
     } catch(err) {
       console.error(err)
+      res.statusCode(500)
     }
     next();
   }
@@ -37,12 +38,13 @@ function getPage(req, res, next) {
 }
 let server = restify.createServer();
 server.use(restify.plugins.queryParser());
+server.use(restify.plugins.bodyParser({ mapParams: true }));
 server.use(cors())
 server.get('/data/:date', getData);
 server.get('/index.html', getPage);
-server.get('/data/:date/:events', setData);
+server.post('/data/:date', setData);
 // server.head('/set-data/:date', getData);
 
-server.listen(process.env.PORT || 80, function() {
+server.listen(process.env.PORT || 8080, function() {
   console.log('%s listening at %s', server.name, server.url);
 });
