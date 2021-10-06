d413315cf35c641d282c383836d988c80543b8f2
Artem Podloboshnikov <ArtemPS777@yandex.ru>
2021-10-06 23:34:01 +0300

    Version 0.1

--- server.js
+++ server.js
@@ -1,31 +1,58 @@
 let restify = require('restify');
 let cors = require('cors');
-const fs = require('fs')
-const fileContents = fs.readFileSync('./data.json', 'utf8');
+const fs = require('fs');
 
 
 function getData(req, res, next) {
   try {
+  
+    const fileContents = fs.readFileSync('./data.json', 'utf8');
     const data = JSON.parse(fileContents)
-    console.log(data);
     res.send((req.params.date == 'all')?JSON.stringify(data):JSON.stringify(data[req.params.date]));
+   
   } catch(err) {
     console.error(err)
   }
   next();
 }
+
 function setData(req, res, next) {
     try {
+
+      const fileContents = fs.readFileSync('./data.json', 'utf8');
       let data = JSON.parse(fileContents);
-      data[req.params.date] = Object.keys(req.body)[0].split('.');
+      console.log(req.params)
+      console.log(req.body)
+      if (req.params.time === undefined)
+      {
+        // console.log(req.body)
+        data[req.params.date] = req.body[req.params.date];
+        // console.log(data)
+      }
+      else
+      {
+        console.log(data);
+        let buf_data = data[req.params.date];
+        for (let i = 0; i < buf_data.length; i++)
+        {
+          if (Object.keys(buf_data[i])[0] == req.params.time)
+          {
+            buf_data[i] = {};
+            buf_data[i][req.params.time] = req.body[req.params.time]
+          }
+        }
+      }
+
       fs.writeFileSync('./data.json', JSON.stringify(data))
-      res.statusCode(200);
+      res.send(JSON.stringify(data));
+
     } catch(err) {
       console.error(err)
-      res.statusCode(500)
+      res.status(500)
     }
     next();
   }
+
 function getPage(req, res, next) {
   const page = fs.readFileSync('./index.html')
   res.writeHead(200, {
@@ -41,10 +68,11 @@ server.use(restify.plugins.queryParser());
 server.use(restify.plugins.bodyParser({ mapParams: true }));
 server.use(cors())
 server.get('/data/:date', getData);
-server.get('/index.html', getPage);
+server.get('/index', getPage);
+server.put('/data/:date/:time', setData);
 server.post('/data/:date', setData);
 // server.head('/set-data/:date', getData);
 
-server.listen(process.env.PORT || 8080, function() {
+server.listen(process.env.PORT || 80, function() {
   console.log('%s listening at %s', server.name, server.url);
 });
